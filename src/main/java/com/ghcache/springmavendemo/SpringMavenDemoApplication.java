358f0c0f1f714a01bed3c9b3bb9f3b93e34046ac
Lovepreet Singh <pdotl@github.com>
2022-07-11 16:51:16 +0530

    Initialize repository

---
+++ src/main/java/com/ghcache/springmavendemo/SpringMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ghcache.springmavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMavenDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghcache/springmavendemo/SpringMavenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghcache.springmavendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
